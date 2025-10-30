b3c9ad3cab7599658f3ed9c6bfa9c7cafda830b1
primrosies <provesialine@gmail.com>
2025-10-29 22:18:57 -0300

    A mini login project made in C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string username;
+string password;
+
+
+Console.WriteLine("Enter an username");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter a password (It must contain at least 8 digits): ");
+password = Console.ReadLine();
+
+if (password.Length >= 8)
+{
+    Console.WriteLine("Login successful!");
+}
+
+else
+{
+    Console.WriteLine("Invalid password!");
+}
