98ea428176cf710f7aaf3d284716cdc2c8805025
primrosies <provesialine@gmail.com>
2025-11-04 21:32:44 -0300

    Clean build files and complete merge

--- obj/Debug/net8.0/Mini Login.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mini Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e874056e7800b49db23c80198727f62478a178")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
